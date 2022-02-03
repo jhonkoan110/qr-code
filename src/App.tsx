72183921a274652761f746c7083526b6121a7c8a
jhonkoan110 <jhonkoan110@gmail.com>
2022-02-03 20:01:02 +0300

    completed

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,10 @@ import './App.css';
 import { QRCodeCard } from './components/QRCodeCard';
 import { FC } from 'react';
 
-const App: FC = () => {
-  const qrcodeBackground = '#2b7dfa';
-  const qrcodeCirclesColor = '#3684fe';
-
-  return (
-    <div className="App">
-      <QRCodeCard />
-    </div>
-  );
-};
+const App: FC = () => (
+  <div className="App">
+    <QRCodeCard />
+  </div>
+);
 
 export default App;
--- src/components/QRCodeCard/QRCodeCard.tsx
+++ src/components/QRCodeCard/QRCodeCard.tsx
@@ -1,6 +1,27 @@
 import { FC } from 'react';
+import QRCode from 'qrcode.react';
 import classes from './QRCodeCard.module.css';
 
 export const QRCodeCard: FC = () => {
-  return <div className={classes.container}>sadad</div>;
+  return (
+    <div className={classes.container}>
+      <div className={classes.QRCodeBackground}>
+        <QRCode
+          value="https://github.com/jhonkoan110"
+          renderAs="svg"
+          bgColor="transparent"
+          fgColor="#fff"
+          level="M"
+          className={classes.QRCode}
+          size={170}
+        />
+        <div className={classes.QRCodeCircle} />
+        <div className={classes.QRCodeCircle} />
+      </div>
+      <h2 className={classes.title}>Improve your front-end skills by building projects</h2>
+      <p className={classes.description}>
+        Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+      </p>
+    </div>
+  );
 };
